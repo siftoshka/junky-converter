32802475c3c181701e980a90452b95285f80da1e
amorphist <azad.mamiyev.98@gmail.com>
2021-10-06 15:18:39 +0200

    Added Application class

---
+++ app/src/main/java/az/siftoshka/junkyconverter/App.kt
@@ -0,0 +1,10 @@
+package az.siftoshka.junkyconverter
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * The application class of the entire app.
+ */
+@HiltAndroidApp
+class App : Application()
